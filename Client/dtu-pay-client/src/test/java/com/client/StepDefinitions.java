27cc388012084d7d00a0342958c475707524d294
Lucas Juel <lucas.juel.sjostrom@gmail.com>
2026-01-08 10:11:40 +0100

    added first cucumber tests to Client

--- Client/dtu-pay-client/src/test/java/com/client/StepDefinitions.java
+++ Client/dtu-pay-client/src/test/java/com/client/StepDefinitions.java
@@ -5,18 +5,40 @@ import io.cucumber.java.en.Then;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StepDefinitions {
-    @Given("The program runs")
-                public void program_runs() {
-            System.out.println("The program runs");
+public class SimpleDTUPaySteps {
+    private Customer customer;
+    private Merchant merchant;
+    private String customerId, merchantId;
+    private SimpleDtuPay dtupay = new SimpleDtuPay();
+    private boolean successful = false;
 
+    @Given("a customer with name {string}")
+    public void aCustomerWithName(String name) {
+        customer = new Customer(name);
+    }
 
-        }
-        @Then("The answer is Hello")
-        public void the_answer_is_hello() {
-            String result = Main.helloFromMain();
-            assert(result.equals("Hello"));
-            System.out.println("Testen bestået: Svaret er Hello");
-        }
+    @Given("the customer is registered with Simple DTU Pay")
+    public void theCustomerIsRegisteredWithSimpleDTUPay() {
+        customerId = dtupay.register(customer);
+    }
+
+    @Given("a merchant with name {string}")
+    public void aMerchantWithName(String name) {
+        merchant = new Merchant(name);
+    }
+
+    @Given("the merchant is registered with Simple DTU Pay")
+    public void theMerchantIsRegisteredWithSimpleDTUPay() {
+        merchantId = dtupay.register(merchant);
+    }
+
+    @When("the merchant initiates a payment for {int} kr by the customer")
+    public void theMerchantInitiatesAPaymentForKrByTheCustomer(Integer amount) {
+        successful = dtupay.pay(amount,customerId,merchantId);
+    }
+
+    @Then("the payment is successful")
+    public void thePaymentIsSuccessful() {
+        assertTrue(successful);
+    }
 }
-
--- Client/dtu-pay-client/src/test/resources/features/helloTest.feature
+++
@@ -1,4 +0,0 @@
-Feature: Hello
-  Scenario: A user wants someone to tell them hello
-    Given The program runs
-    Then The answer is Hello
