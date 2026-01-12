3b589d7bcdcbc082bc513434f6d12f3e9732bc1e
ghostdog <oliverbadike@gmail.com>
2026-01-12 12:02:40 +0100

    Added new SOAP test

--- Client/dtu-pay-client/src/test/java/com/client/RunTests.java
+++ Client/dtu-pay-client/src/test/java/com/client/RunTests.java
@@ -5,13 +5,11 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.client")
 public class RunTests {
 }
