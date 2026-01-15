60bc32409e84a1f1a356c2eb800251463cbe7c02
ghostdog <oliverbadike@gmail.com>
2026-01-15 11:58:28 +0100

    Add initial implementation of payment processing and related DTOs

---
+++ Payment/payment-service/src/main/java/org/g10/DTO/MerchantDTO.java
@@ -0,0 +1,36 @@
+package org.g10.DTO;
+
+public class MerchantDTO {
+    private String firstName;
+    private String lastName;
+    private String cpr;
+    private String bankAccountId;
+
+    public MerchantDTO() {
+    }
+
+    public MerchantDTO(String firstName, String lastName, String cpr, String bankAccountId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cpr = cpr;
+        this.bankAccountId = bankAccountId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public String getBankAccountId() {
+        return bankAccountId;
+    }
+
+}
