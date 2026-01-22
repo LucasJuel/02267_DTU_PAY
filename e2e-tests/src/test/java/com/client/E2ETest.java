1c1dbd04f09d804127525d1dbdd7f454de122ea3
ghostdog <oliverbadike@gmail.com>
2026-01-22 12:13:53 +0100

    Update Jenkinsfile, Makefile, and README to simplify test commands and remove failsafe plugin references

---
+++ e2e-tests/src/test/java/com/client/E2ETest.java
@@ -0,0 +1,19 @@
+package com.client;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.client")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@e2e")
+public class E2ETest {
+}
